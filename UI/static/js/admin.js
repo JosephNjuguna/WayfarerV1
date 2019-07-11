a1c1c1020a0daee69b934e5ab2ae1459b3469b4d
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-11 13:39:22 +0300

    ft(ui): add javascript to display trips div [Starts #167179823]

---
+++ UI/static/js/admin.js
@@ -0,0 +1,16 @@
+//shop div
+var createtripModal = document.getElementById("createTrips");
+var trips = document.getElementById("tripsModal");
+// profile modal button
+var showCreatetrip = document.getElementById("navCreatetrip");
+var showTrips = document.getElementById("navTrips");
+
+showCreatetrip.onclick = function () {
+    createtripModal.style.display = "flex";
+    trips.style.display = "none";
+};
+
+showTrips.onclick = function () {
+    trips.style.display = "flex";
+    createtripModal.style.display = "none";
+};
