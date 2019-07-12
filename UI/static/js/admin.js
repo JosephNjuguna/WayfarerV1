2d19a030dcced52596067dc41ec1070aedc4680d
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-12 14:23:23 +0300

    ft(ui): add javascript to display bookings div [Finish #167251571]

--- UI/static/js/admin.js
+++ UI/static/js/admin.js
@@ -1,9 +1,12 @@
 var createtripModal = document.getElementById("createTrips");
 var trips = document.getElementById("tripsModal2");
+var bookings = document.getElementById("bookingModal");
+
 var tripModal = document.getElementById("tripCreated");
 var showCreatetrip = document.getElementById("navCreatetrip");
 var showTrips = document.getElementById("navTrips");
 var tripCreated = document.getElementById("createTrip");
+var showbooking = document.getElementById("navBookings");
 
 showCreatetrip.onclick = function () {
     createtripModal.style.display = "flex";
@@ -13,8 +16,15 @@ showCreatetrip.onclick = function () {
 showTrips.onclick = function () {
     trips.style.display = "flex";
     createtripModal.style.display = "none";
+    bookings.style.display = "none";
 };
 
 tripCreated.onclick = function () {
     tripModal.style.display = "flex";
+};
+
+showbooking.onclick = function () {
+    bookings.style.display = "flex";
+    createtripModal.style.display = "none";
+    trips.style.display = "none";
 };
